688244974b1db0ca4c98f93d8cd56df61d08ff44
sujoy-chandra-das <dsujoy209@gmail.com>
2025-01-15 11:33:06 +0600

    Remove garbage code

--- src/test/java/testcases/testing.java
+++ src/test/java/testcases/testing.java
@@ -1,27 +1,3 @@
-//package testcases;
-//
-//import pageObjects.HomePage;
-//import base.Base;
-//import org.testng.Assert;
-//import org.testng.annotations.Test;
-//
-//public class testing extends Base {
-//
-//    @Test // Test method to run the test
-//    public void HomepageAppear() throws InterruptedException {
-//
-//        Thread.sleep(10000); // Wait for the page to load
-//        HomePage hp = new HomePage(driver);
-//
-//
-//        // Interact with the Name field
-//        hp.getNameField().sendKeys("Hasan al mamun");
-//        Thread.sleep(5000); // Wait for any action to take effect
-//
-//
-//    }
-//}
-
 package testcases;
 
 import pageObjects.HomePage;
